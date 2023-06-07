0d76cf4fbeba64c8b047478593608c7274ef6901
john <johnthomas222000@gmail.com>
2023-06-07 10:57:00 +0530

    counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import "./styles/styles.css";
-import Img from './assets/images/sample.png';
-import svg  from './assets/images/parachuting-1781587.svg'
+// import Img from "./assets/images/sample.png";
+// import svg from "./assets/images/parachuting-1781587.svg";
+import CLickCounter from "./componnets/CLickCOunter";
 
 export const App = () => {
   return (
     <div>
       <div className="head">React webpack template</div>
-      <img src={Img} />
-      <img src={svg} />
+      <CLickCounter />
     </div>
   );
 };
