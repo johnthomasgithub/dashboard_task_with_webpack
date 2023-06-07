9cead4506e9f92e3b9d557cce70da9956187158f
john <johnthomas222000@gmail.com>
2023-06-07 07:58:02 +0530

    added image plugins to webpack

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,13 @@
+import "./styles/styles.css";
+import Img from './assets/images/sample.png';
+import svg  from './assets/images/parachuting-1781587.svg'
+
 export const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div className="head">React webpack template</div>
+      <img src={Img} />
+      <img src={svg} />
+    </div>
+  );
 };
